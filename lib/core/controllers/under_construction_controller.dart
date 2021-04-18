a3d92bd495ca74067c53f0f9d079155f54e5be52
Rui Miguel Alonso <ruiskas@gmail.com>
2021-04-18 20:04:38 +0200

    Fix: translations

--- lib/core/controllers/under_construction_controller.dart
+++ lib/core/controllers/under_construction_controller.dart
@@ -3,43 +3,41 @@ import 'package:flutter/material.dart';
 import 'package:flutter_gen/gen_l10n/app_localizations.dart';
 
 class UnderConstructionController extends ChangeNotifier {
-  final List<String> _sentences = [
-    "House blowing up builds character.",
-    "Bad Deadpool... Good Deadpool!",
-    "Whose kitty litter did I just shit in?",
-    "Say the magic words, Fat Gandalf.",
-    "Happy Lent.",
-    "Dad?",
-    "This is my most prized possession...",
-    "Guy came in here looking for you. Real Grim Reaper-type. I don't know. Might further the plot.",
-    "If I ever decide to become a crime-fighting shit swizzler, who rooms with a bunch of other little whiners at Neverland Mansion with some creepy, old, bald, Heaven's Gate-looking motherfucker... on that day, I'll send your shiny, happy ass a friend request.",
-    "All the dinosaurs feared the T-Rex.",
-    "McAvoy or Stewart? These timelines are confusing.",
-    "Ever see 127 Hours?",
-    "I want to die a natural death at the age of 102 - like the city of Detroit.",
-    "Please don't make the super suit green... or animated!",
-    "You have something in your teeth.",
-    "He got Ajax from the dish soap!",
-    "You will die alone, if you could die - ideally, for others sake.",
-    "Captain Deadpool! No, just Deadpool.",
-    "Fourth wall break inside of a fourth wall break? That's like... 16 walls!",
-    "You're about to be killed by a Zamboni.",
-    "I bet it feels huge in this hand.",
-    "Have fun at your midnight showing of Blade II.",
-    "Listen Al, if I never see you again, I want you to know that I love you very much. I also buried 1,600 kilos of cocaine somewhere in the apartment -- right next to the cure for blindness. Good luck.",
-    "It's a Big house. It's weird I only ever see two of you. Almost like the studio couldn't afford another X-Man.",
-    "Superhero landing! She's going to do a superhero landing!",
-    "When I'm finished parts will have to grow back you.",
-    "You were droning on!",
-    "You live in a house?",
-    "After a brief adjustment period, and a couple of drinks, it's a face... I'd be happy to sit on.",
-    "You're still here? It's over. Go home! Oh, you're expecting a teaser for Deadpool 2**. Well, we don't have that kind of money. What are you expecting? Sam Jackson showing up in an eyepatch and a saucy little leather number? Go!",
-  ];
+  String getSentence(BuildContext _context) {
+    final List<String> _sentences = [
+      AppLocalizations.of(_context)!.deadpool_message1,
+      AppLocalizations.of(_context)!.deadpool_message2,
+      AppLocalizations.of(_context)!.deadpool_message3,
+      AppLocalizations.of(_context)!.deadpool_message4,
+      AppLocalizations.of(_context)!.deadpool_message5,
+      AppLocalizations.of(_context)!.deadpool_message6,
+      AppLocalizations.of(_context)!.deadpool_message7,
+      AppLocalizations.of(_context)!.deadpool_message8,
+      AppLocalizations.of(_context)!.deadpool_message9,
+      AppLocalizations.of(_context)!.deadpool_message10,
+      AppLocalizations.of(_context)!.deadpool_message11,
+      AppLocalizations.of(_context)!.deadpool_message12,
+      AppLocalizations.of(_context)!.deadpool_message13,
+      AppLocalizations.of(_context)!.deadpool_message14,
+      AppLocalizations.of(_context)!.deadpool_message15,
+      AppLocalizations.of(_context)!.deadpool_message16,
+      AppLocalizations.of(_context)!.deadpool_message17,
+      AppLocalizations.of(_context)!.deadpool_message18,
+      AppLocalizations.of(_context)!.deadpool_message19,
+      AppLocalizations.of(_context)!.deadpool_message20,
+      AppLocalizations.of(_context)!.deadpool_message21,
+      AppLocalizations.of(_context)!.deadpool_message22,
+      AppLocalizations.of(_context)!.deadpool_message23,
+      AppLocalizations.of(_context)!.deadpool_message24,
+      AppLocalizations.of(_context)!.deadpool_message25,
+      AppLocalizations.of(_context)!.deadpool_message26,
+      AppLocalizations.of(_context)!.deadpool_message27,
+      AppLocalizations.of(_context)!.deadpool_message28,
+      AppLocalizations.of(_context)!.deadpool_message29,
+      AppLocalizations.of(_context)!.deadpool_message30,
+    ];
 
-  String getSentence(BuildContext context) {
     var index = Random().nextInt(_sentences.length);
-    //TODO: check internationalization for this messages
-    //AppLocalizations.of(context)!.under_construction,
     return _sentences[index];
   }
 }
