259f8fceceeea6e50d0cf603b3c0c674a104899d
Rui Miguel Alonso <ruiskas@gmail.com>
2021-05-13 19:30:10 +0200

    New: trying to do ui testing

--- test/marvel_app_test.dart
+++ test/marvel_app_test.dart
@@ -1,3 +1,4 @@
+import 'package:flutter/material.dart';
 import 'package:flutter_test/flutter_test.dart';
 import 'package:marvel/marvel_app.dart';
 import 'package:marvel/ui/auth_screen.dart';
@@ -5,27 +6,23 @@ import 'package:marvel/ui/home/home_screen.dart';
 import 'package:marvel/ui/login/login_screen.dart';
 import 'package:marvel_data/marvel_data.dart';
 import 'package:shared_preferences/shared_preferences.dart';
+import 'providers_test.dart';
 
 void main() {
   group('MarvelApp', () {
-    late SharedPreferences sharedPreferences;
-
     setUp(() async {
       SharedPreferences.setMockInitialValues({});
-      sharedPreferences = await SharedPreferences.getInstance();
     });
 
-    testWidgets('renders WeatherAppView', (tester) async {
+    testWidgets('renders MarvelApp AuthScreen', (tester) async {
       await tester.pumpWidget(MarvelApp(
-        prefs: sharedPreferences,
+        prefs: await SharedPreferences.getInstance(),
       ));
       expect(find.byType(AuthScreen), findsOneWidget);
     });
   });
 
   group('AuthScreen', () {
-    late SharedPreferences sharedPreferences;
-
     testWidgets('renders LoginScreen when user unauthenticated',
         (tester) async {
       SharedPreferences.setMockInitialValues({});
