ed687c2f7989c1348d9a9dae8d4ea9d062fdf977
Rui Miguel Alonso <ruiskas@gmail.com>
2021-05-19 11:01:52 +0200

    Fix: modified main and removed old provider di

--- lib/main.dart
+++ lib/main.dart
@@ -1,11 +1,14 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:marvel/marvel_app.dart';
+import 'package:marvel/ui/commons/bloc_observer.dart';
 import 'package:shared_preferences/shared_preferences.dart';
 
 Future<void> main() async {
   WidgetsFlutterBinding.ensureInitialized();
   final prefs = await SharedPreferences.getInstance();
 
+  Bloc.observer = CustomBlocObserver();
   runApp(MarvelApp(
     prefs: prefs,
   ));
--- lib/marvel_app.dart
+++ lib/marvel_app.dart
@@ -1,10 +1,10 @@
 import 'package:flutter/material.dart';
-import 'package:marvel/providers.dart';
+import 'package:flutter_gen/gen_l10n/app_localizations.dart';
+import 'package:marvel/dependency_injection.dart';
 import 'package:marvel/router.dart';
 import 'package:marvel/styles/themes.dart';
 import 'package:marvel/ui/auth_screen.dart';
 import 'package:shared_preferences/shared_preferences.dart';
-import 'package:flutter_gen/gen_l10n/app_localizations.dart';
 
 class MarvelApp extends StatelessWidget {
   const MarvelApp({Key? key, required this.prefs}) : super(key: key);
@@ -13,7 +13,7 @@ class MarvelApp extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return buildMultiProvider(
+    return buildDependencyInjection(
       preferences: prefs,
       widget: MaterialApp(
         home: AuthScreen(),
