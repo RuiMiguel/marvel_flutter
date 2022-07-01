9541efa6f37e9e5fd61c17c00feaf78199e75670
RuiAlonso <rui.alonso@verygood.ventures>
2022-07-01 19:37:00 +0200

    refactor: added stacktrace to exceptions

--- packages/api_client/lib/src/exception/api_exception.dart
+++ packages/api_client/lib/src/exception/api_exception.dart
@@ -1,27 +1,36 @@
 // ignore_for_file: public_member_api_docs
 
 abstract class ApiException implements Exception {
-  const ApiException(this.error);
+  const ApiException(this.error, [this.stackTrace]);
 
   final Object error;
+
+  final StackTrace? stackTrace;
 }
 
 class NetworkException extends ApiException {
-  const NetworkException(super.error);
+  const NetworkException(super.error, [super.stackTrace]);
 
   @override
   String toString() => '[NetworkException] $error';
 }
 
 class ServerException extends ApiException {
-  const ServerException(super.error);
+  const ServerException(super.error, [super.stackTrace]);
 
   @override
   String toString() => '[ServerException] $error';
 }
 
+class AuthenticationException extends ApiException {
+  const AuthenticationException(super.error, [super.stackTrace]);
+
+  @override
+  String toString() => '[AuthenticationException] $error';
+}
+
 class DeserializationException extends ApiException {
-  const DeserializationException(super.error);
+  const DeserializationException(super.error, [super.stackTrace]);
 
   const DeserializationException.emptyResponseBody()
       : super('Empty response body');
