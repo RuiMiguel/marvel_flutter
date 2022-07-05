320d8334396b66deb94ad44ee6eead2be6812c14
RuiAlonso <rui.alonso@verygood.ventures>
2022-07-05 10:03:58 +0200

    refactor: webview section

---
+++ lib/webview/webview_page.dart
@@ -0,0 +1,71 @@
+import 'package:flutter/material.dart';
+import 'package:marvel/home/widget/widget.dart';
+import 'package:marvel/styles/colors.dart';
+import 'package:webviewx/webviewx.dart';
+
+class WebViewPage extends StatefulWidget {
+  const WebViewPage({super.key, required this.url});
+
+  static const String routeName = 'web-view';
+
+  final String url;
+
+  @override
+  WebViewPageState createState() => WebViewPageState();
+}
+
+class WebViewPageState extends State<WebViewPage> {
+  late WebViewXController _controller;
+  bool _isLoading = true;
+
+  Size get screenSize => MediaQuery.of(context).size;
+
+  void _showProgress({bool loading = true}) {
+    setState(() {
+      _isLoading = loading;
+    });
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return WillPopScope(
+      onWillPop: () async {
+        if (await _controller.canGoBack()) {
+          await _controller.goBack();
+          return Future.value(false);
+        } else {
+          return Future.value(true);
+        }
+      },
+      child: Scaffold(
+        appBar: const HeroesAppBar(
+          withActions: false,
+        ),
+        body: Stack(
+          children: [
+            WebViewX(
+              height: screenSize.height,
+              width: screenSize.width,
+              initialContent: widget.url,
+              onWebViewCreated: (controller) => _controller = controller,
+              onPageStarted: (src) => _showProgress(),
+              onPageFinished: (src) => _showProgress(loading: false),
+              navigationDelegate: (navigation) {
+                debugPrint(navigation.content.sourceType.toString());
+                return NavigationDecision.navigate;
+              },
+            ),
+            Visibility(
+              visible: _isLoading,
+              child: const Center(
+                child: CircularProgressIndicator(
+                  color: red,
+                ),
+              ),
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+}
